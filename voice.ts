372d59455156d8ac43770b7dee15ca414f252485
leog <leog91@gmail.com>
2024-10-11 07:01:26 +0200

    voice

--- voice.ts
+++ voice.ts
@@ -1,112 +1,8 @@
-// import {
-//     joinVoiceChannel,
-//     createAudioPlayer,
-//     createAudioResource,
-//     generateDependencyReport,
-//     entersState,
-//     StreamType,
-//     AudioPlayerStatus,
-//     VoiceConnectionStatus,
-//     VoiceConnection,
-// } from "@discordjs/voice";
-
-// import { randomAsset } from "./utils.js";
-// import { Message } from "discord.js"; // Ensure you have the correct Discord.js import
-
-// console.log("++++++++++", generateDependencyReport());
-
-// const player = createAudioPlayer();
-
-// export function playSong(): Promise<void> {
-//     const resource = createAudioResource(
-//         "./assets/audio/giraldo+de+ayer-001.mp3"
-//     );
-
-//     player.play(resource);
-//     console.log("player . play ---");
-
-//     return entersState(player, AudioPlayerStatus.Playing, 5e3).then(() => {
-//         // Do nothing on resolve; just ensure it completes
-//     });
-// }
-
-// export function playSongBis(audio: string): Promise<void> {
-//     const resource = createAudioResource(`./assets/audio/${audio}.mp3`);
-//     player.play(resource);
-
-//     return entersState(player, AudioPlayerStatus.Playing, 5e3).then(() => {
-//         // Do nothing on resolve; just ensure it completes
-//     });
-// }
-
-// export const playRandom = (command: string): Promise<void> => {
-//     const resource = createAudioResource(randomAsset(command));
-//     player.play(resource);
-
-//     return entersState(player, AudioPlayerStatus.Playing, 5e3).then(() => {
-//         // Do nothing on resolve; just ensure it completes
-//     });
-// };
-
-// // Functions to control the audio player
-// export const stop = (): boolean => {
-//     return player.stop(); // This will now return the boolean value
-// };
-
-// export const pause = (): boolean => {
-//     return player.pause(); // This will now return the boolean value
-// };
-
-// export const resume = (): boolean => {
-//     return player.unpause(); // This will now return the boolean value
-// };
-
-// async function connectToChannel(channel: { id: string; guild: { id: string; }; }, adapter: any): Promise<VoiceConnection> {
-//     const connection = joinVoiceChannel({
-//         channelId: channel.id,
-//         guildId: channel.guild.id,
-//         adapterCreator: adapter,
-//     });
-
-//     try {
-//         await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
-//         return connection;
-//     } catch (error) {
-//         connection.destroy();
-//         throw error;
-//     }
-// }
-
-// export const voiceFun = async (message: Message): Promise<void> => {
-//     const channel = message.member?.voice.channel;
-
-//     // Check if guild exists
-//     if (!message.guild) {
-//         message.reply("This command can only be used in a server.");
-//         return;
-//     }
-
-//     if (channel) {
-//         try {
-//             const connection = await connectToChannel(
-//                 channel,
-//                 message.guild.voiceAdapterCreator
-//             );
-
-//             connection.subscribe(player);
-//             console.log("PLAYING NOW");
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     } else {
-//         message.reply("Join a voice channel then try again!");
-//     }
-// };
 
 
 // voice.ts
 import { join } from "path";
-import { createAudioPlayer, joinVoiceChannel, createAudioResource, AudioPlayerStatus, VoiceConnection, AudioPlayer } from "@discordjs/voice";
+import { createAudioPlayer, joinVoiceChannel, createAudioResource, AudioPlayerStatus, VoiceConnection, AudioPlayer, AudioResource } from "@discordjs/voice";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -150,12 +46,16 @@ export const leaveVoiceChannel = () => {
 };
 
 // Function to play a specific song
-export const playSong = async (songName: string, message: Message) => {
+export const playSong = async (songName: string, message: Message, isRandom: boolean = false) => {
 
 
-    const resource = createAudioResource(join(__dirname, `./assets/audio/${songName}.mp3`));
+    let resource: AudioResource<null>
 
-    // const resource = createAudioResource(join(__dirname, `./assets/audio/age/death.mp3`));
+    if (isRandom) {
+        resource = createAudioResource(join(__dirname, `./assets/audio/age/${songName}`));
+    } else {
+        resource = createAudioResource(join(__dirname, `./assets/audio/${songName}.mp3`));
+    }
 
     audioPlayer.play(resource);
 
@@ -180,7 +80,7 @@ export const playSong = async (songName: string, message: Message) => {
 // Function to play a specific song and then perform voice actions
 export const playSongBis = async (songName: string, message: Message) => {
     // console.log("songname", songName)
-    await playSong(songName, message);
+    await playSong(songName, message, false);
     voiceFun(message);
 };
 
@@ -204,12 +104,10 @@ export const resume = (message: Message) => {
 
 // Function to play a random song
 export const playRandom = async (category: string, message: Message) => {
-    const songList = ["song1", "song2", "song3"]; // Add your song logic here
+
     const random = randomAsset("age")
-    console.log(random, ">>>>>")
-    const randomSong = songList[Math.floor(Math.random() * songList.length)];
     //    await playSong(randomSong, message);
-    await playSong(random, message);
+    await playSong(random, message, true);
 };
 
 // Function to perform voice-related actions
