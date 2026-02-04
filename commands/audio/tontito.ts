a4b60ebcbda58d3d69f7aca35a73a673f5b9c4f0
leog <leog91@gmail.com>
2026-02-04 20:48:31 +0000

    more audios, db updated

---
+++ commands/audio/tontito.ts
@@ -0,0 +1,18 @@
+import { defineCommand } from "..";
+import { guilds } from "../../utils/constants";
+import { useVoice } from "../../voice";
+
+export default defineCommand({
+    name: "tontito",
+    type: "AUDIO",
+    description: "",
+    permissions: [
+        { type: "GUILD", ids: [guilds.Bytes, guilds.plll] }
+    ],
+    hidden: true,
+
+    async execute(msg) {
+        const voice = useVoice(msg.guild!.id);
+        await voice.play(msg, "tontito");
+    }
+});
