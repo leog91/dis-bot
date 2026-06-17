7da6dd07663a97e83d8d2286cb34a355bd30d5cf
leog <leog91@gmail.com>
2026-06-17 01:28:19 +0100

    feat: enhance vid command with improved link handling and embed waiting logic

--- commands/text/vid.ts
+++ commands/text/vid.ts
@@ -1,11 +1,10 @@
-import { Message, TextChannel } from "discord.js";
+import { Embed, Message, TextChannel } from "discord.js";
 import { defineCommand } from "..";
 import {
     createVidProgressMessage,
     deleteOriginalMessage,
     getVidSourceInfo,
     resolveVidOutputUrl,
-    sendVidResponse,
     trySendRedditVideo,
     trySendTwitterVideo,
     type VidProgressMessage,
@@ -31,6 +30,34 @@ const playAcknowledgementIfNeeded = async (msg: Message) => {
 };
 
 const getUrlArg = (msg: Message) => msg.content.split(" ").slice(1)[0];
+
+const waitForEmbed = async (
+    message: Message,
+    predicate: (embed: Embed) => boolean,
+    timeoutMs = 5000
+) => {
+    const start = Date.now();
+
+    while (Date.now() - start < timeoutMs) {
+        if (message.embeds.some(predicate)) {
+            return true;
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, 500));
+
+        try {
+            message = await message.fetch();
+        } catch {
+            return false;
+        }
+    }
+
+    return message.embeds.some(predicate);
+};
+
+const isVideoEmbed = (embed: Embed) => Boolean(embed.video?.url);
+
+const isAnyEmbed = () => true;
 
 export default defineCommand({
     name: "vid",
@@ -63,21 +90,37 @@ export default defineCommand({
         await deleteOriginalMessage(msg);
 
         let progress: VidProgressMessage | undefined;
+        let linkMessage: Message | undefined;
 
         try {
             const sourceInfo = getVidSourceInfo(url);
+            const outputUrl = await resolveVidOutputUrl(url, sourceInfo);
+
+            const linkContent = sourceInfo.isTwitterLike
+                ? `[Tweet](<${url}>)\n${outputUrl}`
+                : outputUrl;
+
+            linkMessage = await msg.channel.send(`by ${msg.author}:\n${linkContent}`);
+
+            const hasPreview = sourceInfo.isTwitterLike
+                ? await waitForEmbed(linkMessage, isAnyEmbed)
+                : await waitForEmbed(linkMessage, isVideoEmbed);
+            if (hasPreview) {
+                return;
+            }
 
             if (sourceInfo.isRedditLike) {
                 progress = await createVidProgressMessage(msg, "Preparing Reddit video...");
                 const redditResult = await trySendRedditVideo(msg, url, progress);
                 if (redditResult.sent) {
+                    await linkMessage.delete().catch(() => {});
                     return;
                 }
 
-                const notice = "notice" in redditResult ? redditResult.notice : undefined;
-                const outputUrl = await resolveVidOutputUrl(url, sourceInfo);
-                await progress.update("Couldn't upload the video, sending the link instead...");
-                await sendVidResponse(msg, outputUrl, notice, progress);
+                await progress?.remove();
+                if ("notice" in redditResult && redditResult.notice) {
+                    await msg.channel.send(redditResult.notice);
+                }
                 return;
             }
 
@@ -85,20 +128,17 @@ export default defineCommand({
                 progress = await createVidProgressMessage(msg, "Preparing Twitter video...");
                 const twitterResult = await trySendTwitterVideo(msg, url, progress);
                 if (twitterResult.sent) {
+                    await linkMessage.delete().catch(() => {});
                     return;
                 }
 
-                const outputUrl = await resolveVidOutputUrl(url, sourceInfo);
-                await progress.update("Couldn't upload the video, sending the link instead...");
-                await sendVidResponse(msg, `[Tweet](<${url}>)\n${outputUrl}`, undefined, progress);
+                await progress?.remove();
                 return;
             }
-
-            const outputUrl = await resolveVidOutputUrl(url, sourceInfo);
-            await sendVidResponse(msg, outputUrl);
         } catch (err) {
             console.error(err);
             await progress?.remove();
+            await linkMessage?.delete().catch(() => {});
             await msg.channel.send(`by ${msg.author}:\n${url}`);
         }
     }
